a92e08ade6cfb74e7819af53f34b3304c7f4df26
wojtek <wswistak@student.agh.edu.pl>
2024-01-31 11:57:01 +0100

    divide login route for user, admin

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,15 +1,16 @@
 const {
   adminRegister,
-  login,
   userRegister,
+  userLogin,
+  adminLogin,
 } = require("../controllers/authController");
 
 const router = require("express").Router();
 
 router.route("/admin/register").post(adminRegister);
-router.route("/admin/login").post(login);
+router.route("/admin/login").post(adminLogin);
 
 router.route("/user/register").post(userRegister);
-router.route("/user/login").post(login);
+router.route("/user/login").post(userLogin);
 
 module.exports = router;
