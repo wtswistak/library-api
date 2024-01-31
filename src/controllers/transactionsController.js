d65b4071b02ff5a1474acf5fa0fe5dd6e6c29b68
wojtek <wswistak@student.agh.edu.pl>
2024-01-31 11:55:40 +0100

    refactor controller with prisma

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,78 +1,52 @@
-const { queryDb, client } = require("../utils/queryDb");
+const { getBookById } = require("../services/booksService");
+const {
+  returnBook,
+  borrowBook,
+  getLastTransaction,
+  updateAvailability,
+} = require("../services/transactionsService");
+const { CustomError, errorHandler } = require("../utils/error");
 
-const borrowBook = async (req, res) => {
+const borrowBookCtrl = async (req, res) => {
   const { id: bookId } = req.params;
   const userId = req.user.userId;
 
   try {
-    const isBookExist = await queryDb(
-      `SELECT * FROM books WHERE id=$1`,
-      [bookId],
-      client
-    );
-    if (isBookExist.length === 0) return res.status(400).send("Book not found");
-    if (!isBookExist[0].is_available)
-      return res.status(400).send("Book is not available");
+    const book = await getBookById(bookId);
+    if (!book.isAvailable) throw new CustomError(400, "Book is not available");
 
-    await queryDb(
-      `UPDATE books SET is_available=$1 WHERE id=$2`,
-      ["FALSE", bookId],
-      client
-    );
-    await queryDb(
-      `INSERT INTO transactions(user_id, book_id,action) VALUES($1, $2,$3)`,
-      [userId, bookId, "borrow"],
-      client
-    );
+    await updateAvailability(bookId, false);
 
-    res.status(200).send("Borrow book success");
+    await borrowBook(bookId, userId);
+    res.json({ status: "success", message: "Borrow book success" });
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    errorHandler(error, res);
   }
 };
 
-const returnBook = async (req, res) => {
+const returnBookCtrl = async (req, res) => {
   const { id: bookId } = req.params;
   const userId = req.user.userId;
 
   try {
-    const isBookExist = await queryDb(
-      `SELECT * FROM books WHERE id=$1`,
-      [bookId],
-      client
-    );
-    if (isBookExist.length === 0) return res.status(400).send("Book not found");
-    if (isBookExist[0].is_available)
-      return res.status(400).send("Book is already available");
+    const book = await getBookById(bookId);
+    if (book.isAvailable)
+      throw new CustomError(400, "Book is already returned");
 
-    const lastBorrow = await queryDb(
-      `SELECT * FROM transactions WHERE book_id=$1 AND action=$2 ORDER BY date DESC LIMIT 1`,
-      [bookId, "borrow"],
-      client
-    );
-    if (lastBorrow[0].user_id !== userId)
-      return res.status(400).send("This book is not borrowed by you");
+    const lastTransaction = await getLastTransaction(bookId);
+    if (lastTransaction.userId !== userId)
+      throw new CustomError(400, "Book is borrowed by another user");
 
-    await queryDb(
-      `UPDATE books SET is_available=$1 WHERE id=$2`,
-      ["TRUE", bookId],
-      client
-    );
-    await queryDb(
-      `INSERT INTO transactions(user_id, book_id,action) VALUES($1, $2,$3)`,
-      [userId, bookId, "return"],
-      client
-    );
+    await updateAvailability(bookId, true);
+    await returnBook(bookId, userId, lastTransaction.id);
 
     res.status(200).send("Return book success");
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    errorHandler(error, res);
   }
 };
 
 module.exports = {
-  borrowBook,
-  returnBook,
+  borrowBookCtrl,
+  returnBookCtrl,
 };
